2817137710b28cc5cb56d7080a85981a912d34e3
stenli3108 <stanley.geraldy3108@gmail.com>
2024-07-23 13:00:53 +0700

    Add files via upload

---
+++ tugas php/Hal111-no33.php
@@ -0,0 +1,7 @@
+<?php
+  $siswa = array("Andri","Joko","Sukma","Rina","Sari");
+
+  echo $siswa[2];
+  echo "<br>";
+  echo "Murid itu bernama $siswa[0]";
+?>
---
+++ tugas php/Hal148-no22.php
@@ -0,0 +1,15 @@
+<?php
+$a = 10;
+$a += 5;
+echo $a; 
+echo "<br>";
+
+$a = 55;
+$a /= 5;
+echo $a; 
+echo "<br>";
+
+$a = "Belajar";
+$a .= "PHP";
+echo $a; 
+?>
